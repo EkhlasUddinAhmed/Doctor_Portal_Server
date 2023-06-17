ad7fe8eadfaec8f1b702010b355bbef13f214e3c
unknown <matrixedu14@gmail.com>
2023-06-17 11:38:06 +0600

    Dirname path is rermoved

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const bodyParser=require("body-parser")
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const { availableTreatment } = require("./AllRouters/AvailableTreatment");
 const {
@@ -9,21 +9,22 @@ const {
 } = require("./AllRouters/bookingTreatmentRouter");
 const userRouter = require("./AllRouters/userRouter");
 const doctorRouter = require("./AllRouters/doctorRouter");
-const paymentRouter=require("./AllRouters/paymentRouter");
+const paymentRouter = require("./AllRouters/paymentRouter");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/teeth", availableTreatment);
 app.use("/booking", bookingTreatmentRouter);
 app.use("/new", userRouter);
 app.use("/doctor", doctorRouter);
 app.use(errorHandler);
-app.use(express.static(`${__dirname}/uploads/`));
-app.use("/payment",paymentRouter);
+// app.use(express.static(`${__dirname}/uploads/`));
+app.use(express.static(`/uploads/`));
+app.use("/payment", paymentRouter);
 // Database Connection Starts Here....
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.13y3n.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -57,7 +58,6 @@ function errorHandler(err, req, res, next) {
   } else res.send("Success");
   res.status(500).json({ error: err });
 }
-
 
 // console.log("From Index.js, UUID is:",uuidv4());
 // const { v4: uuidv4 } = require('uuid');
